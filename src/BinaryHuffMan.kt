6c7fe15fdf8350058e7c4111f0846df4a732e5fe
Anthony Phan <anthonyphania216@gmail.com>
2018-11-20 21:03:48 -0600

    Finished tree building and word coding

--- src/BinaryHuffMan.kt
+++ src/BinaryHuffMan.kt
@@ -1,7 +1,24 @@
-class BinaryHuffMan(freq: Int){
+class BinaryHuffMan(freq: Double, word: String? =null){
     var leftNode: BinaryHuffMan? = null
     var rightNode: BinaryHuffMan? = null
     var parentNode: BinaryHuffMan? = null
     var bitValue = 0
     var freqValue = freq
+    var wordValue: String? = word
+
+    fun addLeft(child: BinaryHuffMan){
+        leftNode = child
+        child.parentNode = this
+    }
+    fun addRight(child: BinaryHuffMan){
+        rightNode = child
+        child.parentNode = this
+    }
+    fun changeBit(){
+        bitValue = 1
+    }
+
+    fun hasParent(): Boolean {
+        return (parentNode != null)
+    }
 }
--- src/Huffman Trees.kt
+++ src/Huffman Trees.kt
@@ -4,7 +4,7 @@ import java.io.File
 class Huffman : HuffManTree {
 
     var wordCounter = mutableMapOf<String,Int>()
-    var wordCode = mutableMapOf<String,Int>()
+    var wordCode = mutableMapOf<String,String>()
     var wordCount = 0.0
 
     override fun readFile(fileLocation: String) {
@@ -19,14 +19,36 @@ class Huffman : HuffManTree {
     }
 
     override fun buildCode() {
-        val wordFrequency = mutableListOf<Pair<String?, Double>>()
-        val keyList = wordCounter.keys
+        val wordFrequency = wordCounter.keys.map { Pair(it,wordCounter[it]!!/wordCount) }
+        val holdingRoots = mutableListOf<BinaryHuffMan>()
+        val wordToNode = mutableMapOf<String,BinaryHuffMan>()
 
-        for (i in keyList) {
-            wordFrequency.add(Pair(i, wordCounter[i]!! / wordCount))
+        wordFrequency.forEach { val temp = BinaryHuffMan(it.second,it.first)
+            wordToNode[it.first] = temp
+            holdingRoots.add(temp) }
+
+        holdingRoots.sortedBy { it.freqValue }
+
+        while (holdingRoots.size>1){
+            val p1 = holdingRoots.removeAt(holdingRoots.size-1)
+            val p2 = holdingRoots.removeAt(holdingRoots.size-1)
+            val combinedFreq = p1.freqValue+p2.freqValue
+            val root = BinaryHuffMan(combinedFreq)
+            root.addLeft(p1)
+            root.addRight(p2)
+            p2.changeBit()
+            holdingRoots.add(root)
+            holdingRoots.sortBy { it.freqValue }
         }
-        wordFrequency.sortBy { it.second }
-
+        for (i in wordToNode.keys){
+            var currentNode = wordToNode[i]
+            var code = ""
+            while (currentNode!!.hasParent()){
+                code = currentNode.bitValue.toString() + code
+                currentNode = currentNode.parentNode
+            }
+            wordCode[i] = code
+        }
     }
 
     override fun decode(fileLocation: String) {
@@ -41,7 +63,8 @@ class Huffman : HuffManTree {
 fun main(args: Array<String>) {
     val t = Huffman()
 
-    t.readFile("C:\\Users\\APhan\\Desktop\\war.txt")
-
-    println(t.wordCounter)
+    t.readFile("C:\\Users\\APhan\\Desktop\\41.txt")
+    //println(t.wordCounter)
+    t.buildCode()
+    println(t.wordCode)
 }
